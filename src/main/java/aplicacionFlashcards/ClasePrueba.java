4e1d31dd715722d4a78dcec899ae7ce8853ecb0c
Sergio Pérez Sánchez <sergiopsinformatico@gmail.com>
2019-10-12 12:24:10 +0200

    SonarCloud

--- src/main/java/aplicacionFlashcards/ClasePrueba.java
+++ src/main/java/aplicacionFlashcards/ClasePrueba.java
@@ -2,6 +2,6 @@ package main.java.aplicacionFlashcards;
 
 public class ClasePrueba {
 	public static void main(String[] args) {
-		System.out.println("Prueba ");
+		System.out.println("Prueba SonarCloud");
 	}
 }
