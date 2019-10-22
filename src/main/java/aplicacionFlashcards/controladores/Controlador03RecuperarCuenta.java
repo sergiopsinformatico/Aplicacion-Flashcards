3af06e2d15e7d3a2532ff299c82c76bdd85cda31
Sergio Pérez Sánchez <sergiopsinformatico@gmail.com>
2019-10-23 00:28:36 +0200

    UTF-8

--- src/main/java/aplicacionFlashcards/controladores/Controlador03RecuperarCuenta.java
+++ src/main/java/aplicacionFlashcards/controladores/Controlador03RecuperarCuenta.java
@@ -90,7 +90,7 @@ public class Controlador03RecuperarCuenta {
 				vista.addObject("username", username);
 			}else if(Broker.getInstanciaRecuperarCuenta().existeSolicitudUsuario(username)) {
 				vista = new ModelAndView("vistaRecuperarCuenta");
-				vista.addObject("mensaje", "Solicitó un restablecimiento de la clave, pero el código no es válido");
+				vista.addObject("mensaje", "Solicito un restablecimiento de la clave, pero el codigo no es valido");
 			}else {
 				vista = new ModelAndView("vistaRecuperarCuenta");
 				vista.addObject("mensaje", "El enlace ha expirado. Por favor, vuelva a solicitar la recuperacion de la clave");
