7616b1940709cf06303d385ce28e58ffe11df9f1
Sergio Pérez Sánchez <sergiopsinformatico@gmail.com>
2020-01-26 11:34:47 +0100

    Mejora Wiki

--- src/main/java/aplicacionflashcards/controladores/Controlador11Wiki.java
+++ src/main/java/aplicacionflashcards/controladores/Controlador11Wiki.java
@@ -16,4 +16,39 @@ public class Controlador11Wiki {
 	public ModelAndView verWikiProyecto(HttpServletRequest request, HttpServletResponse response) {
 		return new ModelAndView("vistaWiki");
 	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s0")
+	public ModelAndView verWikiProyectoSprintCero(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint0");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s1")
+	public ModelAndView verWikiProyectoSprintUno(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint1");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s2")
+	public ModelAndView verWikiProyectoSprintDos(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint2");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s3")
+	public ModelAndView verWikiProyectoSprintTres(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint3");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s4")
+	public ModelAndView verWikiProyectoSprintCuatro(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint4");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s5")
+	public ModelAndView verWikiProyectoSprintCinco(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint5");
+	}
+	
+	@GetMapping(value = "/desarrolloProyecto-s6")
+	public ModelAndView verWikiProyectoSprintSeis(HttpServletRequest request, HttpServletResponse response) {
+		return new ModelAndView("vistaWikiSprint6");
+	}
 }
